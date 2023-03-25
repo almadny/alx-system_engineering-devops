416efdccd5b4029c0d884380c08ba303f0c1ee17
Almadny <Almadny2u@gmail.com>
2023-03-25 09:11:39 +0000

    Added Puppet files as follows;
    0-create_a_file.pp ---- Creates a file
    1-install_a_package ---- Install flask version 2.1.0
    2-execute_a_command ---- Run command to kill a process

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
+#Installs flask using pip3
+
 package {'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
+#Execute a command to kill the killmenow program
+
 exec {'killmenow':
   path    => '/usr/bin',
   command => 'pkill -f killmenow',
