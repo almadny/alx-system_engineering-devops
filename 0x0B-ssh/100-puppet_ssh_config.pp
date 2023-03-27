c367cbb9647e27614d7c439e41834d7c6962ea85
Almadny <Almadny2u@gmail.com>
2023-03-27 02:52:51 +0000

    Task 100 Added

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#Update configuration file to disable password auth and chnge id file
+
+file { '/etc/ssh/ssh_config':
+	ensure => 'file',
+}
+file_line {'Turn off passwd auth':
+	path => '/etc/ssh/ssh_config',
+	line => 'PasswordAuthentication no',
+	require => File['/etc/ssh/ssh_config'],
+}
+file_line {'Declare identity file':
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school',
+	require => File ['/etc/ssh/ssh_config'],
+}
