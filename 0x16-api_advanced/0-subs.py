ba9a0eca937991eacaa3b643375494df6856ca11
Almadny <Almadny2u@gmail.com>
2023-05-10 06:49:51 +0000

    Fix Pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ A Function to return the count of subscriber for a given subreddit """
 import requests
+
+
 def number_of_subscribers(subreddit):
     """Returns the numbers of subscribers for the subreddit"""
+
     address = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    hder = {'User-Agent':'My_ALX_API_Client'}
+    hder = {'User-Agent': 'My_ALX_API_Client'}
     try:
         data = requests.get(address, headers=hder, allow_redirects=False)
         data = data.json()
         sub = data['data']['subscribers']
         return sub
-    except:
+    except Exception:
         return 0
