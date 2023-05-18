9c5e2e738ad01402701ed9c3abf6990b44a93e78
Almadny <Almadny2u@gmail.com>
2023-05-18 17:38:39 +0000

    modified restart command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,5 +4,6 @@ exec {'change_open_file_limit':
         command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx",
 }
 exec {'restart_nginx':
-        command => "/usr/sbin/service nginx restart"
+        command => "nginx restart"
+	path => '/etc/init.d/'
 }
