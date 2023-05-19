650c6640827a7b71fc92a9331d373deafa480ad6
Almadny <Almadny2u@gmail.com>
2023-05-19 01:54:49 +0000

    Added some modifications

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # Manifest to change number of open files by a worker process
-exec { 'change_open_file_limit':
+
+exec { 'fix-nginx-limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path 	  => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart the nginx server
-exec { 'restart_nginx':
+# Restart the nginx
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
