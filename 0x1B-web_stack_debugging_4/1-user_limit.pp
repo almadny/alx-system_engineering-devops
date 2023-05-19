7934556ed7fafba5cb9f62a7c743abbb398ea87d
Almadny <Almadny2u@gmail.com>
2023-05-19 02:00:05 +0000

    Fixed Task 1 quotes

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # Puppet Manifest to increase the file open limit for holberton user
 
-exec {'Increase-hard':
-  command => "/bin/sed -i 's/5/200/' /etc/security/limits.conf",
+exec { 'increase-hard-limit':
+  command => '/bin/sed -i "s/5/200/" /etc/security/limits.conf',
 }
 
-exec {'Increase-soft':
-  command => "/bin/sed -i 's/4/190/' /etc/security/limits.conf",
+exec { 'increase-soft-limit':
+  command => '/bin/sed -i "s/4/190/" /etc/security/limits.conf',
 }  
