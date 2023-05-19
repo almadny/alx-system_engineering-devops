9a2e1e5b2555d0f1d9c66235b35ca6677b701d00
Almadny <Almadny2u@gmail.com>
2023-05-19 01:22:58 +0000

    Fix indentation

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Manifest to change number of open files by a worker process
 
 exec {'change_open_file_limit':
-        command => "sed -i 's/15/4000/' /etc/default/nginx",
-	path => "/usr/local/bin/:/bin/"
+  command => "sed -i 's/15/4000/' /etc/default/nginx",
+  path 	  => "/usr/local/bin/:/bin/"
 }
 exec {'restart_nginx':
-        command => "nginx restart",
-	path => "/etc/init.d/"
+  command => "nginx restart",
+  path    => "/etc/init.d/"
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
-# Puppet Manifest to increase the hard and soft file open limit for holberton user
+# Puppet Manifest to increase the file open limit for holberton user
 
 exec {'Increase-hard':
-	command => "/bin/sed -i 's/5/200/' /etc/security/limits.conf",
+  command => "/bin/sed -i 's/5/200/' /etc/security/limits.conf",
 }
 
 exec {'Increase-soft':
-	command => "/bin/sed -i 's/4/190/' /etc/security/limits.conf",
+  command => "/bin/sed -i 's/4/190/' /etc/security/limits.conf",
 }  
