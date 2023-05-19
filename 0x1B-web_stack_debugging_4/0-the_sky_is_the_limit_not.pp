80651775014775f9e04ef7f27af33af098c504bf
Almadny <Almadny2u@gmail.com>
2023-05-19 01:32:17 +0000

    Fix indentations

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 # Manifest to change number of open files by a worker process
-# Increase Open file limits
 exec {'change_open_file_limit':
-  command => "sed -i 's/15/4000/' /etc/default/nginx",
+  command => 'sed -i "s/15/4000/" /etc/default/nginx',
   path 	  => "/usr/local/bin/:/bin/"
 }
 
 # Restart the nginx server
 exec {'restart_nginx':
-  command => "nginx restart",
-  path    => "/etc/init.d/"
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
