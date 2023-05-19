ceb58d94cb337fa0182cf5c11fdc350d4121fd49
Almadny <Almadny2u@gmail.com>
2023-05-19 01:49:10 +0000

    Fix indentations

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 exec { 'change_open_file_limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path 	  => '/usr/local/bin/:/bin/',
-}
+} ->
 
 # Restart the nginx server
 exec { 'restart_nginx':
